9a3782886b82570542850b9fdd32ea2f5e2e2531
XuyangZhao20 <97706848+XuyangZhao20@users.noreply.github.com>
2022-12-09 01:44:01 -0800

    week 4

---
+++ main.py
@@ -0,0 +1,20 @@
+import pickle
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    prediction = model.predict([[request.form.get('distance')]])
+    output=round(prediction[0],2)
+    return render_template('index.html', prediction_text = f'The estimated airfare is: {output}USD')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
